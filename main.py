aaa56a7f230a211c061d1cc45fc8987eef6ffc3c
jopezizi <kaariainenjoona@gmail.com>
2025-12-10 11:22:07 +0200

    Refactor code structure for improved readability and maintainability

--- main.py
+++ main.py
@@ -4,23 +4,37 @@ class Game:
     def __init__(self):
         pygame.init()
 
-        self.load_images()
-        self.run()
-
         self.screen = pygame.display.set_mode((800, 600))
+        self.screen_rect = self.screen.get_rect()
         pygame.display.set_caption("Level Game")
         self.clock = pygame.time.Clock()
         
-        self.loop()
+        self.load_images()
+        self.run()
+
 
     def load_images(self):
-        self.images=[]
-        self.filenames =[
-            "hahmo",
-        ]
+        self.images={}
+        bg = pygame.image.load("tausta.jpg").convert()
+        bg = pygame.transform.scale(bg, (self.screen_rect.width, self.screen_rect.height))
 
-        for filename in self.filenames:
-            self.images.append(pygame.image.load(nimi+".png"))
+        self.images["tausta"] = bg
 
     def run(self):
-        
+        runs = True
+        while runs:
+            for tapahtuma in pygame.event.get():
+                if tapahtuma.type == pygame.QUIT:
+                    runs = False
+            self.screen_update()
+            self.clock.tick(60)
+        pygame.quit()
+        exit()
+
+    def screen_update(self):
+        self.screen.blit(self.images["tausta"], (0,0))
+
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    Game()
